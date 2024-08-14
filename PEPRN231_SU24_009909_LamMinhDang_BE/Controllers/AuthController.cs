95a94f09af609df0d48503a4810fb06c6f9380b4
Lâm Minh Đăng <2603lamminhdang@gmail.com>
2024-08-14 20:30:54 +0700

    update

--- PEPRN231_SU24_009909_LamMinhDang_BE/Controllers/AuthController.cs
+++ PEPRN231_SU24_009909_LamMinhDang_BE/Controllers/AuthController.cs
@@ -29,11 +29,11 @@ namespace PEPRN231_SU24_009909_LamMinhDang_BE.Controllers
             }
             return Ok(_tokenGenerator.GenerateToken(exitedUser));
         }
-        [HttpGet("T3st")]
-        public async Task<IActionResult> test()
-        {         
-            return Ok("Code Pipline Working");
-        }
+        //[HttpGet("T3st")]
+        //public async Task<IActionResult> test()
+        //{         
+        //    return Ok("Code Pipline Working");
+        //}
 
     }
 }
